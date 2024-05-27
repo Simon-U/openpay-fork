6fdb94284cfd616a95bd12f2063bd75da354d1c4
Simon Unterbusch <simonunterbusch@unterbusch.com>
2024-05-26 22:06:56 -0600

    Update util.py

--- openpay/util.py
+++ openpay/util.py
@@ -11,5 +11,5 @@ def utf8(value):
     if isinstance(value, bytes):
         return value
     if isinstance(value, str):
-        return value.encode('utf-8')
+        return value.encode("utf-8")
     return value
